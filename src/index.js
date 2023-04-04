20b1f00ad06f3fb70c6438e63e3f5ae4e25929d2
tpercival01 <thomaspercival@live.co.uk>
2023-04-04 21:04:25 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Switch from "./routerSwitch";
 
 const rootElement = document.getElementById("root");
@@ -8,8 +8,8 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter base='/'>
       <Switch />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
--- src/routerSwitch.js
+++ src/routerSwitch.js
@@ -1,15 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import CountryDetails from "./components/countryDetails";
 
 const Switch = () => {
   return (
-    <>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/countries/:i" element={<CountryDetails />} />
+        <Route exact path={process.env.PUBLIC_URL + "/"} element={<App />} />
+        <Route path='/countries/:i' element={<CountryDetails />} />
       </Routes>
-    </>
+    </Router>
   );
 };
 
